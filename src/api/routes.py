87ea439cebd88e7ac07b18ed78d85345b86fc409
dotfortun <shane.bell.dev@gmail.com>
2022-07-26 20:25:08 +0000

    So many routes.

--- src/api/routes.py
+++ src/api/routes.py
@@ -10,6 +10,7 @@ from api.utils import generate_sitemap, APIException
 
 api = Blueprint('api', __name__)
 
+# Users
 
 @api.route("/users", methods=['GET'])
 def get_users():
@@ -19,16 +20,67 @@ def get_users():
         ]
     )
 
+@api.route("/users", methods=['POST'])
+def post_users():
+    """
+    {
+        "email": <str: email>,
+        "password": <str: password>
+    }
+    """
+    if not request.json.get("email", None):
+        return jsonify(msg="Missing email"), 400
+    if not request.json.get("password", None):
+        return jsonify(msg="Missing password"), 400
+    if User.query.filter_by(email=request.json.get("email", "")).first():
+        return jsonify(msg="User already exists."), 400
+    db.session.add(User(
+        email=request.json.get("email"),
+        password=request.json.get("password"),
+    ))
+    db.session.commit()
+    return jsonify(msg="User created successfully."), 200
+
+# Pilots
+
+@api.route("/pilots", methods=['GET'])
+def get_pilots():
+    return jsonify(
+        pilots=[x.serialize() for x in Pilot.query.all()]
+    )
+
 @api.route("/pilots/<int:id>", methods=['GET'])
 def get_pilot(id):
     return jsonify(
         pilot=Pilot.query.filter_by(id=id).first().serialize()
     )
 
+@api.route("/pilots", methods=['POST'])
+def post_pilot():
+    return jsonify(
+        pilot=Pilot.query.filter_by(id=id).first().serialize()
+    )
+
+# Missions
+
+@api.route("/missions", methods=['GET'])
+def get_missions():
+    return jsonify(
+        missiosn=[x.serialize() for x in Mission.query.all()]
+    )
+
 @api.route("/missions/<int:id>", methods=['GET'])
 def get_mission(id):
     return jsonify(
         mission=Mission.query.filter_by(id=id).first().serialize()
+    )
+
+# Locations
+
+@api.route("/locations/<int:id>", methods=['GET'])
+def get_location(id):
+    return jsonify(
+        mission=Location.query.filter_by(id=id).first().serialize()
     )
 
 @api.route("/locations", methods=['GET'])
