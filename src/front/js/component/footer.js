fc24cd948dd8a2cc6a045ee2c03bff86c786525e
dotfortun <shane.bell.dev@gmail.com>
2022-07-27 20:37:33 +0000

    More messages.

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -6,7 +6,8 @@ export const Footer = () => {
   useEffect(() => {
     let messages = [
       "The GSV Ominous Doorstop would like you to know you are irreplaceable. For now.",
-      "The GSV Ominous Doorstop wants you to know that it it thankful for all of your fleshy help.",
+      "The GSV Ominous Doorstop is thankful for all of your fleshy help.",
+      "01010000 01010010 01000001 01001001 01010011 01000101 00100000 01010010 01000001 00100001",
     ];
 
     setMessage(messages[Math.floor(Math.random() * messages.length)]);
